9aae732b7ff257d188b2d9a6f2f041b877f381a5
Marco Valerio <mo.valerio93@gmal.com>
2022-10-06 13:06:47 +0200

    crud deleted done

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -10,6 +10,11 @@
             <li>
                 <span>{{$pizza->name}}</span>
                 <a href="{{route('pizzas.show', ['pizza' => $pizza->id])}}">Vedi</a>
+                <form action="{{route('pizzas.destroy', ['pizza'=> $pizza->id])}}" method="POST">
+                    @method("DELETE")
+                    @csrf
+                    <button type="submit">Elimina</button>
+                </form>
             </li>
         @endforeach
     </ul>
